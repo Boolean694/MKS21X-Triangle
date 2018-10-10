a2822543ec7a42a18eae60fe7faa8e400bccc349
Julian Wong <jwong01@stuy.edu>
2018-10-10 19:48:39 -0400

    add constructors

--- Triangle.java
+++ Triangle.java
@@ -3,7 +3,15 @@ public class Triangle {
     private Point b;
     private Point c;
     
-    public Triangle(Point a, Point b, Point c) {
-    
+    public Triangle(Point an, Point bn, Point cn) {
+		a = new Point(an.getX(), an.getY());
+		b = new Point(bn.getX(), bn.getY());
+		c = new Point(cn.getX(), cn.getY());
     }
+	public Triangle(double ax, double ay, double bx, double by, double cx, double cy) {
+		a = new Point(ax,ay);
+		b = new Point(bx,by);
+		c = new Point(cx,cy);
+	}
+	
 }
