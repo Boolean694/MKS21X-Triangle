25c2d0628eea108f841d8c168921bf6f4b6130bb
Julian Wong <jwong01@stuy.edu>
2018-10-10 20:52:02 -0400

    finish writing triangle and point classes

---
+++ Point.java
@@ -0,0 +1,20 @@
+public class Point {
+	private double x;
+	private double y;
+	public Point(double xc, double yc) {
+		x = xc;
+		y = yc;
+	}
+	public Point(Point p) {
+		x = p.x;
+		y = p.y;
+	}
+	public double getX() {return x;}
+	public double getY() {return y;}
+	public double distOtherPt(Point a) {
+		return Math.sqrt((Math.abs(x - a.getX()) * Math.abs(x - a.getX())) + (Math.abs(y - a.getY()) * Math.abs(y - a.getY())));
+	}
+	public double distTwoPts(Point a, Point b) {
+		return Math.sqrt((Math.abs(b.getX() - a.getX()) * Math.abs(b.getX() - a.getX())) + (Math.abs(b.getY() - a.getY()) * Math.abs(b.getY() - a.getY())));
+	}
+}
--- Triangle.java
+++ Triangle.java
@@ -13,5 +13,21 @@ public class Triangle {
 		b = new Point(bx,by);
 		c = new Point(cx,cy);
 	}
+	public Point getVertex(int vp) {
+		if(vp == 0) {return a;}
+		if(vp == 1) {return b;}
+		if(vp == 2) {return c;}
+	}
+	public double getPerimeter() {
+		return a.distOtherPt(b) + b.distOtherPt(c) + c.distOtherPt(a);
+	}
+	public void setVertex(int n, Point p) {
+		if(n == 0){a = new Point(p.getX(), p.getY());}
+		if(n == 1){b = new Point(p.getX(), p.getY());}
+		if(n == 2){c = new Point(p.getX(), p.getY());}
+	}
+	public String toString() {
+		return "Triangle: A:(" + a.getX() + "," + a.getY() + ") B: (" + b.getX() + "," + b.getY() + ") C: (" + c.getX() + "," + c.getY + ")";
+	}
 	
 }
